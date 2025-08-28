bc4eb6e07f7ffc90065272df07e654a8425b7754
Ram Lakhan <ramlakhan.mobiledev@gmail.com>
2025-08-28 12:43:03 +0530

    setup hilt

--- app/src/main/java/com/rl/financedot/MainActivity.kt
+++ app/src/main/java/com/rl/financedot/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.rl.financedot.ui.theme.FinanceDotTheme
+import com.rl.financedot.presentation.ui.theme.FinanceDotTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/rl/financedot/presentation/ui/theme/Type.kt
+++ app/src/main/java/com/rl/financedot/presentation/ui/theme/Type.kt
@@ -1,4 +1,4 @@
-package com.rl.financedot.ui.theme
+package com.rl.financedot.presentation.ui.theme
 
 import androidx.compose.material3.Typography
 
