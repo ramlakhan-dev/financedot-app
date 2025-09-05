635ae2057affaf268f4c6cddfa7bb47d26c727f5
Ram Lakhan <ramlakhan.mobiledev@gmail.com>
2025-09-05 12:46:44 +0530

    initial commit

---
+++ app/src/main/java/com/rl/financedot/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.rl.financedot
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.rl.financedot.presentation.ui.theme.FinanceDotTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FinanceDotTheme {
+
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/rl/financedot/presentation/ui/theme/Type.kt
@@ -0,0 +1,5 @@
+package com.rl.financedot.presentation.ui.theme
+
+import androidx.compose.material3.Typography
+
+val AppTypography = Typography()
